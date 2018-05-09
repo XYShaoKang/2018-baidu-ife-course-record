3394e9eb9f65d2abb639b2e6f511e5f8fa7eaa0e
ShaoKang <zengwenjie03@gmail.com>
2018-05-09 12:44:42 +0800

    No.8 - 把 AE 动画转换成 Web 原生动画

---
+++ ife-course/designer-college/code/NO08/js/index.js
@@ -0,0 +1,49 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json'
+})
+
+
+var animations = [];
+
+animations.push(bodymovin.loadAnimation({
+    container: document.getElementById('tutor'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'data/tutor.json'
+}))
+animations.push(bodymovin.loadAnimation({
+    container: document.getElementById('compass'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'data/compass.json'
+}))
+animations.push(bodymovin.loadAnimation({
+    container: document.getElementById('example'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'data/example.json'
+}))
+animations.push(bodymovin.loadAnimation({
+    container: document.getElementById('api'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: 'data/api.json'
+}))
+$.each($('.san-hover>div'), function (index, el) {
+    $(el).hover(
+        function () {
+            animations[index].play();
+        },
+        function () {
+            animations[index].stop();
+        }
+    )
+})
